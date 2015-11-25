3df87e836a8139cd8e48ccca9e294cfdca909275
denzildoyle <yell@denzildoyle.me>
2015-11-25 13:52:46 -0400

    Load images from database

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -26,21 +26,11 @@
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-6 col-md-6 col-xs-12 thumb">
-					<a href="<?= base_url() . "public/" ?>img/FFP_1.JPG"><img src="<?= base_url() . "public/" ?>img/preview/FFP_1.jpg" alt=""></a>
-				</div>
-				<div class="col-lg-6 col-md-6 col-xs-12 thumb">
-					<a href="<?= base_url() . "public/" ?>img/FFP_2.jpg"><img src="<?= base_url() . "public/" ?>img/preview/FFP_2.jpg" alt=""></a>
-				</div>
-				<div class="col-lg-6 col-md-6 col-xs-12 thumb">
-					<a href="<?= base_url() . "public/" ?>img/FFP_3.jpg"><img src="<?= base_url() . "public/" ?>img/preview/FFP_3.jpg" alt=""></a>
-				</div>
-				<div class="col-lg-6 col-md-6 col-xs-12 thumb">
-					<a href="<?= base_url() . "public/" ?>img/FFP_4.jpg"><img src="<?= base_url() . "public/" ?>img/preview/FFP_4.jpg" alt=""></a>
-				</div>
-				<div class="col-lg-6 col-md-6 col-xs-12 thumb">
-					<a href="<?= base_url() . "public/" ?>img/FFP_5.jpg"><img src="<?= base_url() . "public/" ?>img/preview/FFP_5.jpg" alt=""></a>
-				</div>
+			   	<?php foreach ($photos as $photo):?>
+					<div class="col-lg-6 col-md-6 col-xs-12 thumb">
+						<a href="<?= base_url() . "public/" ?>img/FFP_1.JPG"><img src="<?= base_url() . "public/img/preview/" . $photo['name'] . ".jpg" ?>" alt="<?= $photo['description']; ?>"></a>
+					</div>
+	          	<?php endforeach;?>
 			</div>
 		</div>
 		<!-- Latest compiled and minified JavaScript -->
