92e46596778d95efdb8695c27a67e17e970b6692
denzildoyle <yell@denzildoyle.me>
2015-11-26 12:05:38 -0400

    Add license view

---
+++ application/views/license_view.php
@@ -0,0 +1,8 @@
+<?= $this->load->view('layout/header'); ?>
+	<body>
+		<div class="container">
+			<div class="col-md-6 col-md-offset-3">
+				<p>All photos published on Free Food are licensed under Creative Commons Zero which means you can copy, modify, distribute and use the photos for free, including commercial purposes, without asking permission from or providing attribution to the photographer or Unsplash. Questions? <a href="mailto:fresh@freshlysqzd.com">Get in touch with us.</a></p>
+			</div>
+		</div>
+<?= $this->load->view('layout/footer'); ?>
