6ce744905b57e4f4c728ad07b6539b287c3a51af
denzildoyle <yell@denzildoyle.me>
2015-11-26 12:11:57 -0400

    Add link logo to home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -2,8 +2,9 @@
 	<body>
 		<div class="container header">
 			<div class="col-md-6 col-md-offset-3">
-				<h2 class="text-center">Free Food Photography</h2>
+				<a class="logo" href="<?= base_url(). ""; ?>"><h2 class="text-center">Free Food Photography</h2></a>
 				<h6 class="text-center">MADE BY <a href="www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
+
 				<p class="text-center"><b>Free (<a href="<?= base_url() . "license" ?>" class="link"> do whatever you want </a>) high-resolution food photos.</b> I take a lot of food photos and never really do anything with them, so here you go.</p>
 				<center><a href="<?= base_url(). "submit"; ?>" class="btn btn-primary" role="button">Upload Photo <i class="icon icon-upload-cloud-1"></i></a></center>
 			</div>
--- application/views/submit_view.php
+++ application/views/submit_view.php
@@ -1,7 +1,10 @@
 <?= $this->load->view('layout/header'); ?>
 	<body>
-		<div class="container">
+		<div class="container header">
 			<div class="col-md-6 col-md-offset-3">
+				<a class="logo" href="<?= base_url(). ""; ?>"><h2 class="text-center">Free Food Photography</h2></a>
+				<h6 class="text-center">MADE BY <a href="www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
+
 				<p>We'll review your photo for publishing and if <a class="link" href="">your submission is accepted</a>, your work will be distributed for free under the <a class="link" href="">Free Food Photography license</a>.
 				<br><small>Please only upload photos <a class="link" href="">that you own the rights to.</a></small></p>
 				<center><a href="<?= base_url(). ""; ?>" class="btn btn-primary" role="button">Add a Photo</a></center>
