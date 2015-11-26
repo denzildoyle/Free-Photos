be88ef800aca8e9f2b30287636b84db3473d8b09
denzildoyle <yell@denzildoyle.me>
2015-11-26 10:55:57 -0400

    Create header and footer template

---
+++ application/views/layout/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>Free Food - Download <?= $photo->name; ?> </title>
+
+	    <link rel="image_src" href="http://www.freshlysqzd.com/freefood/img/fb_thumb_main.png" />
+	    <meta property="og:title" content="Free Food" />
+	    <meta property="og:type" content="website" />
+	    <meta property="og:image" content=".<?= base_url() . "public/" ?>img/fb_thumb_main.png" />
+	    <meta property="og:description" content="" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/style.css">
+
+		<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/animation.css">
+		<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/fontello-codes.css">
+		<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/fontello-embedded.css">
+		<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/fontello.css">
+
+		<!--[if lte IE 7]>
+			<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/fontello-ie7-codes.css">
+			<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/fontello-ie7.css">
+		<![endif]-->
+	</head>
