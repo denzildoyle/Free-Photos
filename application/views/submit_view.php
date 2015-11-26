da982695cb0f8f071cad9d1dcb985ae288f80d82
denzildoyle <yell@denzildoyle.me>
2015-11-26 11:59:37 -0400

    Change gray class to link class

--- application/views/submit_view.php
+++ application/views/submit_view.php
@@ -2,9 +2,9 @@
 	<body>
 		<div class="container">
 			<div class="col-md-6 col-md-offset-3">
-				<p>We'll review your photo for publishing and if <a href="">your submission is accepted</a>, your work will be distributed for free under the <a href="">Free Food Photos license</a>.
-				<br><small>Please only upload photos <a href="">that you own the rights to.</a></small></p>
-				<center><a href="<?= base_url(). ""; ?>" class="btn btn-primary" role="button">Add a Photo</a></center>
+				<p>We'll review your photo for publishing and if <a class="link" href="">your submission is accepted</a>, your work will be distributed for free under the <a href="">Free Food Photos license</a>.
+				<br><small>Please only upload photos <a class="link" href="">that you own the rights to.</a></small></p>
+				<center><a class="link" href="<?= base_url(). ""; ?>" class="btn btn-primary" role="button">Add a Photo</a></center>
 			</div>
 		</div>
 <?= $this->load->view('layout/footer'); ?>
