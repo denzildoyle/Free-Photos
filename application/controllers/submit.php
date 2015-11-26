d82bc154eedb368417725cee1e418cfd015a07ab
denzildoyle <yell@denzildoyle.me>
2015-11-26 13:45:20 -0400

    Add submit button and controller

--- application/controllers/submit.php
+++ application/controllers/submit.php
@@ -2,6 +2,32 @@
 
 class Submit extends CI_Controller {
 	public function index(){
-		$this->load->view('submit_view');
+		$this->load->view('submit_view', array('error' => ' ' ));
+	}
+
+	function do_upload(){
+		$config['upload_path'] = './public/img';
+		$config['allowed_types'] = 'jpg';
+
+		$this->load->library('upload', $config);
+		$this->upload->initialize($config);
+
+		if ($this->upload->do_upload('userPhoto')){
+			//add uploaded files to array
+			$image = $this->upload->data();
+
+			//add data to array
+			$photo = array(
+			  'img' => $image['file_name']
+			);
+
+			// $this->application_model->addPhoto($photo);
+			// $this->session->set_flashdata('msg', 'photo added.');
+			// $this->index();
+		} else{
+			// $this->session->set_flashdata('msg', $this->upload->display_errors());
+			// $this->index();
+			echo $this->upload->display_errors();
+		}
 	}
 }
--- application/views/submit_view.php
+++ application/views/submit_view.php
@@ -7,7 +7,13 @@
 
 				<p>We'll review your photo for publishing and if <a class="link" href="">your submission is accepted</a>, your work will be distributed for free under the <a class="link" href="">Free Food Photography license</a>.
 				<br><small>Please only upload photos <a class="link" href="">that you own the rights to.</a></small></p>
-				<center><a href="<?= base_url(). ""; ?>" class="btn btn-primary" role="button">Add a Photo</a></center>
+
+				<?= $error;?>
+				<?= form_open_multipart('submit/do_upload');?>
+					<center><input type="file" name="userPhoto" size="20" /></center>
+					<center><input type="submit" value="Add a Photo" class="btn btn-primary" role="button"></center>
+
+				</form>
 			</div>
 		</div>
 <?= $this->load->view('layout/footer'); ?>
