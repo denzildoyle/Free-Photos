c938b691be9f41be13856df588496398b3673bad
denzildoyle <yell@denzildoyle.me>
2015-11-26 11:56:11 -0400

    Add submit view

---
+++ application/views/submit_view.php
@@ -0,0 +1,10 @@
+<?= $this->load->view('layout/header'); ?>
+	<body>
+		<div class="container">
+			<div class="col-md-6 col-md-offset-3">
+				<p>We'll review your photo for publishing and if <a href="">your submission is accepted</a>, your work will be distributed for free under the <a href="">Free Food Photos license</a>.
+				<br><small>Please only upload photos <a href="">that you own the rights to.</a></small></p>
+				<center><a href="<?= base_url(). ""; ?>" class="btn btn-primary" role="button">Add a Photo</a></center>
+			</div>
+		</div>
+<?= $this->load->view('layout/footer'); ?>
