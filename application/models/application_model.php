e0792855e94cae73b924d492989e6ce28e063282
denzildoyle <yell@denzildoyle.me>
2015-11-27 09:19:32 -0400

    Add function to model to add new photo

--- application/models/application_model.php
+++ application/models/application_model.php
@@ -4,6 +4,7 @@ class Application_model extends CI_Model{
 
     public function __construct(){
     }
+
 	public function getPhotos(){
         $this->db->where(array('approved' => '1'));
 	    $query = $this->db->get('photos');
@@ -15,5 +16,10 @@ class Application_model extends CI_Model{
         $this->db->where( $data );
         $query = $this->db->get('photos');
         return $query->row(0);
+    }
+
+    public function setUser($photo){
+        $this->db->insert('photos', $photo);
+        return $photo;
     }
 } //end application_model
