24981204c89732d3844839bdb9f7aa72c45faa67
denzildoyle <yell@denzildoyle.me>
2015-11-29 16:23:53 -0400

    Auto load form validation lib

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','image_lib');
+$autoload['libraries'] = array('database','session','image_lib', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
