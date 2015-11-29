33aea12e579f3e80f728c435891e01f98a3b5698
denzildoyle <yell@denzildoyle.me>
2015-11-29 16:23:16 -0400

    Add form validation to submit view

--- application/views/submit_view.php
+++ application/views/submit_view.php
@@ -8,15 +8,32 @@
 				<p>We'll review your photo for publishing and if <a class="link" href="">your submission is accepted</a>, your work will be distributed for free under the <a class="link" href="<?= base_url() . "license" ?>">Free Food Photography license</a>.
 				<br><small>Please only upload photos <a class="link" href="">that you own the rights to.</a></small></p>
 
-				<?= $error;?>
+				<?php if ($this->session->flashdata('message')) : ?>
+					<div class="alert alert-success">
+						<?= $this->session->flashdata('message'); ?>
+					</div>
+				<?php endif; ?>
 				<?= form_open_multipart('submit/do_upload');?>
 					<div class="form-group">
-						<input type="text" name="name" class="form-control" placeholder="Name">
 					</div>
-					<div class="form-group">
-						<textarea name="description" class="form-control" placeholder="Photo Description"></textarea>
+					<?php if(form_error('name') != '') echo "<div class=\"form-group error\">"; else echo "<div class=\"form-group\">"; ?>
+						<span class="help-inline"><?= form_error('name'); ?></span>
+						<input type="text" name="name" class="form-control" value="<?= set_value('name'); ?>" placeholder="name">
 					</div>
-					<input type="file" name="userPhoto" size="20" />
+
+					<?php if(form_error('description') != '') echo "<div class=\"form-group error\">"; else echo "<div class=\"form-group\">"; ?>
+						<span class="help-inline"><?= form_error('description'); ?></span>
+						<textarea name="description" class="form-control" placeholder="photo description"><?= set_value('description'); ?></textarea>
+					</div>
+
+					<?php if(form_error('photo') != '') echo "<div class=\"form-group error\">"; else echo "<div class=\"form-group\">"; ?>
+						<span class="help-inline"><?= form_error('photo'); ?></span>
+						<input type="file" name="photo" value="<?= set_value('photo'); ?>" size="20" />
+					</div>
+
+
+					<p class="text-right"><small>Please enter information in all fields.</small></p>
+
 					<center><input type="submit" value="Submit" class="btn btn-primary" role="button"></center>
 				</form>
 			</div>
