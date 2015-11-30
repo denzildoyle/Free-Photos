4b2de0c942b36dd61684f2b754a3f775a22b5cdc
denzildoyle <yell@denzildoyle.me>
2015-11-30 18:48:41 -0400

    Update git ignore

---
+++ application/config/email.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'mail.caribbeanideas.com';
+$config['smtp_port'] = 587;
+$config['smtp_user'] = 'tng@caribbeanideas.com';
+$config['smtp_pass'] = 'TNGAdm1n$';
+$config['wordwrap'] = False;
+$config['mailtype'] = 'html';
