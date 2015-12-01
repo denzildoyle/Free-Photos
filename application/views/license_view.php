6b397b3f2776a8bbf31aa587fad5b61c64eaa85f
denzildoyle <yell@denzildoyle.me>
2015-12-01 17:56:00 -0400

    Fix link made by freshly squeezed designs

--- application/views/license_view.php
+++ application/views/license_view.php
@@ -3,7 +3,7 @@
 		<div class="container header">
 			<div class="col-md-6 col-md-offset-3">
 				<a class="logo" href="<?= base_url(). ""; ?>"><h2 class="text-center">Free Food Photography</h2></a>
-				<h6 class="text-center">MADE BY <a href="www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
+				<h6 class="text-center">MADE BY <a href="http://www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
 
 				<p>All photos published on Free Food are licensed under Creative Commons Zero which means you can copy, modify, distribute and use the photos for free, including commercial purposes, without asking permission from or providing attribution to the photographer or Unsplash. Questions? <a href="mailto:fresh@freshlysqzd.com">Get in touch with us.</a></p>
 			</div>
--- application/views/submit_view.php
+++ application/views/submit_view.php
@@ -3,7 +3,7 @@
 		<div class="container header">
 			<div class="col-md-6 col-md-offset-3">
 				<a class="logo" href="<?= base_url(). ""; ?>"><h2 class="text-center">Free Food Photography</h2></a>
-				<h6 class="text-center">MADE BY <a href="www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
+				<h6 class="text-center">MADE BY <a href="http://www.freshlysqzd.com">FRESHLY SQUEEZED DESIGNS</a></h6>
 
 				<p>We'll review your photo for publishing and if <a class="link" href="">your submission is accepted</a>, your work will be distributed for free under the <a class="link" href="<?= base_url() . "license" ?>">Free Food Photography license</a>.
 				<br><small>Please only upload photos <a class="link" href="">that you own the rights to.</a></small></p>
