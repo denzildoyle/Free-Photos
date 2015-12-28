f9354499301a138ed3ef2dba14d7e7722069440f
denzildoyle <yell@denzildoyle.me>
2015-12-28 16:08:52 -0400

    Change social image to .png

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -31,7 +31,7 @@
 		<meta name="twitter:url" property="og:url" content="http://freshlysqzd.com/freefood/">
 		<meta name="twitter:title" property="og:title" content="Free Food Photography">
 		<meta name="twitter:description" property="og:description" content="Free do whatever you want high-resolution food photos.">
-		<meta name="twitter:image" property="og:image" content="<?= base_url() . "public/img/" ?>social-img.jpg">
+		<meta name="twitter:image" property="og:image" content="<?= base_url() . "public/img/" ?>social-img.png">
 
 		<?php include_once("analyticstracking.php") ?>
 	</head>
