02ce827c98b3b27f00179c85834b9937e37204cb
denzildoyle <yell@denzildoyle.me>
2016-01-01 09:53:36 -0400

    Add share functionality to links (move code to main.js)

--- application/views/download_view.php
+++ application/views/download_view.php
@@ -10,11 +10,11 @@
 
 		<div class="overlay">
 			<footer>
-				<a class="download" href="<?= base_url() . "public/img/" . $photo->path; ?>" download>Download</a>
+				<a class="download" onClick="ga('send', 'event', 'button', 'click', 'download');" href="<?= base_url() . "public/img/" . $photo->path; ?>" download>Download</a>
 				<ul class="share-icons">
 		 		<li><a class="icon-link" onClick="ga('send', 'event', 'social-icon', 'click', 'web-link');"></a></li>
-		 		<li><a class="icon-twitter" onClick="ga('send', 'event', 'social-icon', 'click', 'twitter');"></a></li>
-		 		<li><a class="icon-facebook-1" onClick="ga('send', 'event', 'social-icon', 'click', 'facebook');"></a></li>
+		 		<li><a class="icon-twitter" onClick="twitter()" ></a></li>
+		 		<li><a class="icon-facebook-1" onClick="facebook()"></a></li>
 		 	</ul>
 			<!-- <i class="demo-icon icon-spin5 animate-spin"></i> -->
 		 	<!-- <li><a class="demo-icon icon-upload-cloud-1"></a></li> -->
