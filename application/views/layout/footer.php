fc8caa6d443d5a490e24d2b70301179123d7e93c
denzildoyle <yell@denzildoyle.me>
2016-01-01 09:54:32 -0400

    Add main.js reference to footer

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,5 @@
 		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+		<script src="<?= base_url(). "public/js/"; ?>main.js"></script>
 	</body>
 </html>
