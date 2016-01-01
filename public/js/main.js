aede7b822d2a65e4c6d573c06a93bac263069373
denzildoyle <yell@denzildoyle.me>
2016-01-01 09:55:44 -0400

    Add new image

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+function twitter(){
+   window.open( "http://twitter.com/share?text=" + "Download amazing free food photography." + "&related=freshlysqzd,denzildoyle&via=freshlysqzd&hashtags=freefoodphotography", 'twitter-share');
+   ga('send', 'event', 'social-icon', 'click', 'twitter');
+}
+
+function facebook(){
+   window.open( "http://www.facebook.com/sharer.php?u=" + "http://www.freshlysqzd.com/freefood");
+	ga('send', 'event', 'social-icon', 'click', 'facebook');
+}
