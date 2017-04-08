5f8fc43b8b6cc392f9c81b15e5b643e24cd4cc88
denzildoyle <yell@denzildoyle.me>
2017-04-08 11:18:01 -0400

    Clean up api controller

--- application/controllers/api/v1.php
+++ application/controllers/api/v1.php
@@ -1,21 +1,7 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-// This can be removed if you use __autoload() in config.php OR use Modular Extensions
 require APPPATH . '/libraries/REST_Controller.php';
 
-/**
- * This is an example of a few basic user interaction methods you could use
- * all done with a hardcoded array
- *
- * @package         CodeIgniter
- * @subpackage      Rest Server
- * @category        Controller
- * @author          Phil Sturgeon, Chris Kacerguis
- * @license         MIT
- * @link            https://github.com/chriskacerguis/codeigniter-restserver
- */
 class V1 extends REST_Controller {
 
     function __construct(){
